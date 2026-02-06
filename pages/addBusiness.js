1275afeb9b4dfcf9bdf331125d59e3d7ee9c8421
Naledi Motsoikha <116162583+NalediPrettyGirl@users.noreply.github.com>
2026-02-06 22:11:00 +0200

    Remove duplicate imports and clean up code

--- pages/addBusiness.js
+++ pages/addBusiness.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const express = require('express');
-const router = express.Router();
 const { db } = require('../firebase');
 
 // Add Business Endpoint
@@ -82,3 +80,4 @@ router.get('/businesses/check/:email', async (req, res) => {
 
 
 module.exports = router;
+
