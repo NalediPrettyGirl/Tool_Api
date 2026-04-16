fe7b8a7fd14f964ce7f7602ebed678cad2ab2712
Naledi Motsoikha <116162583+NalediPrettyGirl@users.noreply.github.com>
2026-04-16 08:34:34 +0200

    Add files via upload

--- api.js
+++ api.js
@@ -4,11 +4,25 @@ const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
+//const serviceAccount = require( './serviceAccountKey.json'); // Your downloaded key
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+
+// Set up Express app
 const app = express();
 const port = process.env.PORT || 3000;
 
-// 1. Initialize DB
-require('./database');
+// Middleware to parse JSON bodies
+app.use(express.json());
+app.use(cors());
+
+// Serve static HTML files
+//app.use(express.static(path.join(__dirname, '..')));
 
 // 2. Middleware
 app.use(cors({
@@ -41,7 +55,7 @@ app.use('/api', require('./pages/shopAuth'));
 app.use('/api', require('./pages/addBusiness'));
 app.use('/api/dashboard', require('./pages/businessDashboard'));
 app.use('/api/public', require('./pages/productDetails'));
-// app.use('/api/admin', require('./pages/admin')); // Add if exists
+app.use('/api/admin', require('./pages/admin'));
 
 // 5. Health Check
 app.get('/health-check', (req, res) => {
